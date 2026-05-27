54ac9ef83ceee0a32891efc120cf7e13e056cdff
Shiroishiii <isaacdantas2503@gmail.com>
2026-05-26 21:09:25 -0300

    configuração do front e inicio da landing page

---
+++ dev_office/pages/Landing/index.tsx
@@ -0,0 +1,12 @@
+
+const Landing = () => {
+  return (
+    <div className="flex min-h-screen bg-[#1A2238]">
+      <header>
+        <h1 className="text-[#C4C0FF] font-bold text-5xl">DevOffice</h1>
+      </header>
+    </div>
+  )
+}
+
+export default Landing
--- dev_office/src/main.tsx
+++ dev_office/src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Landing from '../pages/Landing'
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<Landing />
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
